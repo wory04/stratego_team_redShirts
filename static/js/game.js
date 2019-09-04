dd291e693397e8dae4b70aa0f024c652c6e535cb
Márk Paróczi <mark.paroczi@gmail.com>
2019-09-04 11:13:49 +0200

    Add Drag&Drop logic

---
+++ static/js/game.js
@@ -0,0 +1,62 @@
+function drag(event) {
+}
+
+function dragend(event) {
+    event.target.style.opacity = "";
+}
+
+function dragover(event) {
+    event.preventDefault()
+}
+
+function dragleave(event) {
+    if (event.target.classList.contains('dropzone')) {
+        event.target.dataset.dropzone = 'false'
+    }
+}
+
+function dragstart(event) {
+    if (event.target.classList.contains('soldier')) {
+        event.target.dataset.dragged = 'true';
+    }
+}
+
+function dragenter(event) {
+    let dragged = document.querySelector("[data-dragged='true']");
+    if (event.target.classList.contains('dropzone') && dragged !== null) {
+        event.target.dataset.dropzone = 'true';
+    }
+}
+
+function drop(event) {
+    let dragged = document.querySelector("[data-dragged='true']");
+    if (dragged !== null) {
+        if (event.target.classList.contains('dropzone') && event.target.innerHTML == 0) {
+            event.preventDefault();
+            dragged.parentNode.removeChild(dragged);
+            event.target.appendChild(dragged);
+
+        }
+    }
+    if (dragged) {
+        dragged.dataset.dragged = 'false';
+        dragged = null;
+        event.target.dataset.dropzone = 'false';
+    }
+}
+
+function setArmy() {
+    document.addEventListener('drag', drag, false);
+    document.addEventListener('dragstart', dragstart, false);
+    document.addEventListener('dragend', dragend, false);
+    document.addEventListener('dragover', dragover, false);
+    document.addEventListener('dragenter', dragenter, false);
+    document.addEventListener('dragleave', dragleave, false);
+    document.addEventListener("drop", drop, false);
+}
+
+function main() {
+    setArmy();
+}
+
+main();
